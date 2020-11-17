ddbd564f5a1efa7ab6b7194430b3e5a570b6c93f
David <ds90289@yahoo.com>
2020-11-17 13:58:51 +0800

    OAuth2Strategy requires a clientID option

--- server.js
+++ server.js
@@ -1,12 +1,104 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const port = process.env.PORT || 3000;
+var express = require('express');
+var passport = require('passport');
+var util = require('util');
 
-server.listen(port, () => {
-  console.log('Server listening at port %d', port);
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+var BnetStrategy = require('passport-bnet').Strategy;
+//var GitHubStrategy = require('passport-github').Strategy;
+
+//var GITHUB_ID = process.env.GITHUB_ID;
+//var GITHUB_SECRET = process.env.GITHUB_SECRET;
+var BNET_ID = process.env.BNET_ID;
+var BNET_SECRET = process.env.BNET_SECRET;
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+// Use the GitHubStrategy within Passport.
+/*
+passport.use(
+  new GitHubStrategy(
+    { clientID: GITHUB_ID,
+      clientSecret: GITHUB_SECRET,
+      callbackURL: "https://localhost/auth/github/callback" },
+    function(accessToken, refreshToken, profile, done) {
+      process.nextTick(function () {
+        return done(null, profile);
+      });
+    })
+);
+*/
+
+// Use the BnetStrategy within Passport.
+passport.use(
+  new BnetStrategy(
+    { clientID: process.env.BNET_ID,
+      clientSecret: process.env.BNET_SECRET,
+      scope: "wow.profile sc2.profile",
+      callbackURL: "https://localhost/auth/bnet/callback" },
+    function(accessToken, refreshToken, profile, done) {
+      process.nextTick(function () {
+        return done(null, profile);
+      });
+    })
+);
+
+var app = express();
+
+// configure Express
+app.use(cookieParser());
+app.use(session({ secret: 'blizzard',
+                  saveUninitialized: true,
+                  resave: true }));
+
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.get('/auth/github', passport.authenticate('github'));
+/*
+app.get('/auth/github/callback',
+        passport.authenticate('github', { failureRedirect: '/' }),
+        function(req, res){
+          res.redirect('/');
+        });
+*/
+app.get('/auth/bnet',
+        passport.authenticate('bnet'));
+
+app.get('/auth/bnet/callback',
+        passport.authenticate('bnet', { failureRedirect: '/' }),
+        function(req, res){
+          res.redirect('/');
+        });
+
+app.get('/', function(req, res) {
+  if(req.isAuthenticated()) {
+    var output = '<h1>Express OAuth Test</h1>' + req.user.id + '<br>';
+    if(req.user.battletag) {
+      output += req.user.battletag + '<br>';
+    }
+    output += '<a href="/logout">Logout</a>';
+    res.send(output);
+  } else {
+    res.send('<h1>Express OAuth Test</h1>' +
+             '<a href="/auth/bnet">Login with Bnet</a>');
+  }
+});
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
