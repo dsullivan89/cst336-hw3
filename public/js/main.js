ce3e4917ac79ad0dd44e0147ca5561a0803180a6
David <ds90289@yahoo.com>
2020-11-18 13:46:32 +0800

    added button to retrieve data

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
 
+	//have to generate a token as they expire in 24 hours
+	async function generateToken() {
+		let baseURL = "https://us.battle.net/oauth/token";
+		let response = await fetch(baseURL,{
+			 method:'POST',
+			 headers: {
+				  'Authorization':'Basic NGY0NDA4NWE5NGNhNDU5ZDlmNjlmZTUwNGMxMTE2Y2Y6SmJHSWxBc253VmpJY2NMRFhlQ2tiQjFDTDk1ZjZnNHk=',
+				  'Host':'us.battle.net',
+				  'Content-Type':'application/x-www-form-urlencoded',
+				  'Accept':'application/json'
+			 }, //headers
+			 body:"grant_type=client_credentials"
+		}); //fetch
+		let data = await response.json();
+		return data.access_token;
+  } //generateToken
 	init();
+
+	$("#infoButton").click(function() {
+		get();
+	});
 
 	function get() {
 		return new Promise((resolve, reject) => {
