2534b3122b4d5f26a5c355122c74eeb51321fd60
David <ds90289@yahoo.com>
2020-11-18 16:50:52 +0800

    updated main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
 
-	/*
-	var url = "https://us.battle.net/oauth/token";
-
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", url);
-
-	xhr.setRequestHeader("Authorization", "Basic ODk4YzRmMTNhODg5NGMxYWJiYjdhODEwMzA3MGQ4YmY6eWszMXFQOVdkc1lhS0VjbDJoNGoxNEtkUGFONVhqNEE=");
-	xhr.setRequestHeader("Content-Type", "application/json");
-	// xhr.setRequestHeader("Content-Length", "0");
 	
+	function httpGet(url, callback)
+	{
+		url = "/initialData";
 
-	xhr.onreadystatechange = function () {
-   	if (xhr.readyState === 4) {
-   	   console.log(xhr.status);
-   	   console.log(xhr.responseText);
-   	}};
+		var xhr = new XMLHttpRequest();
+
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4 && xhr.states == 200) {
+				console.log(xhr.status);
+				console.log(xhr.responseText);
+				callback(xhr.responseText);
+			}};
+
+		xhr.open("GET", url, true);
+		xhr.send(null);
+	}
 
 	var data = { "grant_type":"client_credentials" };
 
 	xhr.send(JSON.stringify(data));
 	// xhr.send();
-	*/
-
 
 }); // document ready
