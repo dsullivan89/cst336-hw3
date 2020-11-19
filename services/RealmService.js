eab96bc9f055aa93421fae88bbd8fe912197542f
David <ds90289@yahoo.com>
2020-11-19 10:41:52 +0800

    edit

--- services/RealmService.js
+++ services/RealmService.js
@@ -36,6 +36,8 @@ class RealmService {
         //const encodedCharacterName = encodeURIComponent(characterName);
 		  //const realmNameSlug = slug(realmName);
 		  
+
+		  //https://us.api.blizzard.com/data/wow/search/connected-realm?namespace=dynamic-classic-us&locale=en_US&status.type=UP&realms.timezone=America%2FNew_York&orderby=id&_page=1&access_token=USBgoEKse2GFzIXCIOCXTzM4vZfqpfldDm
 		  
 		  const realmListDocumentURL = 'https://us.api.blizzard.com/data/wow/connected-realm/index?namespace=dynamic-classic-us&locale=en_US&access_token=USBgoEKse2GFzIXCIOCXTzM4vZfqpfldDm';
 		   
