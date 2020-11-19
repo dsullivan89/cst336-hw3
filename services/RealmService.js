c542c99d5ad647fe25f22dfc0099021d954093ae
David <ds90289@yahoo.com>
2020-11-19 11:18:13 +0800

    edited url

--- services/RealmService.js
+++ services/RealmService.js
@@ -39,7 +39,7 @@ class RealmService {
 
 		  //https://us.api.blizzard.com/data/wow/search/connected-realm?namespace=dynamic-classic-us&locale=en_US&status.type=UP&realms.timezone=America%2FNew_York&orderby=id&_page=1&access_token=USBgoEKse2GFzIXCIOCXTzM4vZfqpfldDm
 		  
-		  const realmListDocumentURL = 'https://us.api.blizzard.com/data/wow/connected-realm/index?namespace=dynamic-classic-us&locale=en_US&access_token=USBgoEKse2GFzIXCIOCXTzM4vZfqpfldDm';
+		  const realmListDocumentURL = 'https://us.api.blizzard.com/data/wow/connected-realm/index'; // ?namespace=dynamic-classic-us&locale=en_US&access_token=USBgoEKse2GFzIXCIOCXTzM4vZfqpfldDm';
 		   
 		  // `https://us.api.blizzard.com/data/wow/search/realm?namespace=${namespace}&locale=${locale}&timezone=America%2FNew_York&orderby=${orderbyField}&_page=${pageNumber}&access_token=${oauthToken}`;
 
